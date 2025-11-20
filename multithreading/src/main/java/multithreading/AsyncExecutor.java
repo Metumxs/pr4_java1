9b5cffcf0e0a0bbcbba9f0f9592540cb775ce2a8
Metumxs <fromuash@gmail.com>
2025-11-20 21:19:29 +0200

    Initial commit

---
+++ multithreading/src/main/java/multithreading/AsyncExecutor.java
@@ -0,0 +1,14 @@
+package multithreading;
+
+
+public class AsyncExecutor
+{
+    public void execute(final Runnable task)
+    {
+        final var thread = new Thread(task);
+
+        System.out.println("Running a new thread: " + thread.getId());
+
+        thread.start();
+    }
+}
